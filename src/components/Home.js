cbd6022d04f09ce480b32a33d9a5c1eac73d1c77
Christian Parrish <christian.parrish@ttu.edu>
2021-04-13 17:17:51 -0500

    Graphs n more stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
 //import Announcement from './Announcement'
 import StockGraph from './StockGraph'
+import StockGraphInput from './StockGraphInput';
 
 /*<Announcement
 				title="Example Thing"
 				text="GME TO THE MOON"			
 				timestamp="2020-12-23 23:12:32.4567"
 				userId="Jackmerius Tacktheritrix"
+				<StockGraph tickerId="1" timespanId='1'/>
 			/>*/
 
 const Home = () => {
 	return (
 		<div>
-			<StockGraph/>
+			<StockGraphInput/>
+			
 		</div>
 	);
 }
